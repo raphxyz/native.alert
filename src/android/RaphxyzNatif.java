4d2b5b230d5cfc29c1d60b6ee650317a6e18fd61
raphxyz <rv@raph.be>
2015-07-05 12:11:24 +0200

    Ajout Lisez-moi.txt

---
+++ src/android/RaphxyzNatif.java
@@ -0,0 +1,38 @@
+package com.raphxyz.natif;
+ 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import android.R;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
+ 
+public class RaphxyzNatif extends CordovaPlugin {
+ 
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if("alert".equals(action)){
+      final String content = args.getString(0);
+      showAlert(content);
+      callbackContext.success();
+      return true;
+    } else {
+      callbackContext.error("RaphxyzNatif."+action+" not found !");
+      return false;
+    }
+  }
+ 
+  private void showAlert(String content){
+    // see http://developer.android.com/guide/topics/ui/dialogs.html
+    AlertDialog.Builder alertDialog = new AlertDialog.Builder(this.cordova.getActivity(), AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+    alertDialog.setTitle("Alert");
+    alertDialog.setMessage(content);
+    alertDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener(){
+      public void onClick(DialogInterface dialog, int id){
+        // User clicked OK button
+      }
+    });
+    alertDialog.show();
+  }
+}
