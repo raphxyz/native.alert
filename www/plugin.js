c72f6dc93e567a79b18c7907b746a6a25ccf331c
raphxyz <rv@raph.be>
2016-01-08 14:56:08 +0100

    Update plugin.js
    Add promtPassword function

--- www/plugin.js
+++ www/plugin.js
@@ -10,10 +10,13 @@ var RaphxyzNatif = {
 	promt: function(title, content, onSuccess, onError){
 		exec(onSuccess, onError, 'RaphxyzNatif', 'prompt', [title, content]);
 	},
+	promtPassword: function(title, content, onSuccess, onError){
+		exec(onSuccess, onError, 'RaphxyzNatif', 'promptPassword', [title, content]);
+	},
 	beep: function(count) {
-        var defaultedCount = count || 1;
-        exec(null, null, "RaphxyzNatif", "beep", [ defaultedCount ]);
-    }
+        	var defaultedCount = count || 1;
+        	exec(null, null, "RaphxyzNatif", "beep", [ defaultedCount ]);
+    	}
 };
 
 module.exports = RaphxyzNatif;
