f679638301f20dd9d8a966124577e66a51a44f90
raphxyz <rv@raph.be>
2016-01-08 16:04:36 +0100

    Update RaphxyzNatif.java

--- src/android/RaphxyzNatif.java
+++ src/android/RaphxyzNatif.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.text.InputType
+import android.text.InputType;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
